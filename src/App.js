3646aa4cab27302311eec65707d054e1c9b3ac7c
Erasmus Mensah <blacharles00@gmail.com>
2021-01-27 12:15:06 +0000

    new commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Home from './Components/Home'
+import Home from './Components/Home/Home';
 
 function App() {
   return (
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MenuRoundedIcon from '@material-ui/icons/MenuRounded';
+
+function Home() {
+    return (
+        <div className="navbar">
+            <span className="navbar-toggle" id="js-navbar-toggler">
+                <MenuRoundedIcon />
+            </span>
+        </div>
+    )
+}
+
+export default Home
