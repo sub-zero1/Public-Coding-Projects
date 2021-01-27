ae6a28030f60b4b0778caf5db9aac6bf7799a54a
Erasmus Mensah <blacharles00@gmail.com>
2021-01-27 12:43:53 +0000

    deleted files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import React from 'react'
 import './App.css';
-import Home from './Components/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      
     </div>
   );
 }
